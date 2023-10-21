444dd5ec12dca39d72864aa20f0942a2df0c6e26
Martina-Suriano <martinasuriano15@gmail.com>
2023-10-21 20:39:17 +0200

    file sum commit

---
+++ sum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+    int somma = 0;
+
+    cout << "Inserisci una sequenza di numeri interi positivi. Inserisci 0 per calcolare la somma." << endl;
+
+    do {
+        cout << "Inserisci un numero: ";
+        cin >> numero;
+
+        if (numero > 0) {
+            somma += numero;
+        } else if (numero < 0) {
+            cout << "Il numero inserito non è positivo. Inserisci un numero positivo." << endl;
+        }
+    } while (numero != 0);
+
+    cout << "La somma dei numeri inseriti è: " << somma << endl;
+
+    return 0;
+}
